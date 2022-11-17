7927ce6bf6f8985862eea18c56af353795032053
Recyclingbottle <hoholeo382@gmail.com>
2022-11-17 12:37:47 +0900

    초기 커밋.

---
+++ WPFControls/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFControls
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnAdd(object sender, RoutedEventArgs e)
+        {
+            string str = $"{Singer.Text}(이)가 부른 {Genre.Text} 장르의 '{Song_Title.Text}'가 추가됨.\n";
+            Result.Text += str;
+        }
+    }
+}
